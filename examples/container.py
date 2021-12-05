45ea11dfdac6ec28a8ba0b2bbc3f327f1ea7a26c
Flaviu Vadan <flaviuvadan@gmail.com>
2021-12-05 09:53:28 -0500

    Add container-based task example (#22)
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

---
+++ examples/container.py
@@ -0,0 +1,15 @@
+"""This example showcases how to run a container, rather than a Python, function, as the payload of a task in Hera"""
+from hera.v1.task import Task
+from hera.v1.workflow import Workflow
+from hera.v1.workflow_service import WorkflowService
+
+# TODO: replace the domain and token with your own
+ws = WorkflowService('my-argo-server.com', 'my-auth-token')
+w = Workflow('fv-testing', ws)
+
+# notice the placeholder `lambda: _`, for now this is required because
+# func is a required parameter, by design. Perhaps this can be made
+# `Optional[Callable]` in the future
+t = Task('cowsay', lambda: _, image='docker/whalesay', command=['cowsay', 'foo'])
+w.add_task(t)
+w.submit()
