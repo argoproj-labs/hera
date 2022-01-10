2bde86b406a99d16f20752a0668e14ced67d6685
flaviuvadan <flaviuvadan@gmail.com>
2022-01-09 19:10:02 -0500

    Add Argo SDK dependency to `dependency_links` field rather than `install_requires` because of PyPi requirements/pypa twine
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

--- setup.py
+++ setup.py
@@ -36,13 +36,16 @@ setup(
     include_package_data=True,
     python_requires='>=3.7',
     install_requires=[
-        # TODO: argo_workflows did not make it into a release that would have published an official
-        # TODO: PyPi version. Once that happens, which is likely in 3.2+, this dependency can be changed safely
-        "argo_workflows @ git+https://github.com/argoproj/argo-workflows@master#subdirectory=sdks/python/client",
         "pydantic",
         "python-dateutil",
         "urllib3",
         "certifi"
+    ],
+    dependency_links=[
+        # TODO: argo_workflows did not make it into a release that would have published an official
+        # TODO: PyPi version. Once that happens, which is likely in 3.2+, this dependency can be changed safely
+        # TODO: Also, move this to `install_requires`
+        "git+https://github.com/argoproj/argo-workflows@master#subdirectory=sdks/python/client"
     ],
     zip_safe=False
 )
