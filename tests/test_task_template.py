9ad48569eaf943b8ecfc580f7c0938e933e6d802
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2022-05-03 14:36:48 +0200

    test_task_template: additional tests with TaskTemplate(...).task()
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

--- tests/test_task_template.py
+++ tests/test_task_template.py
@@ -11,14 +11,32 @@ with open("tests/test_task.py", "r") as file:
         if line.startswith("from ") or line.startswith("import ")
     ]
     for i in imports:
-        # print(i)
         exec(i)
 
 
-# Get all tests defined in test_task.py and modify to use TaskTemplate.
+# Get all test names defined in test_task.py.
 _test_func_names = [test for test in test_task.__dir__() if test.startswith("test")]
+
+# Modify all tests to use TaskTemplate.
 for test_name in _test_func_names:
     f = test_task.__getattribute__(test_name)
     lines = inspect.getsource(f)
     modified_lines = lines.replace("Task(", "TaskTemplate(")
     exec(modified_lines)
+
+# Modify all tests to use TaskTemplate(...).task
+for test_name in _test_func_names:
+    f = test_task.__getattribute__(test_name)
+    lines = inspect.getsource(f)
+
+    def_line = next((line for line in lines.splitlines() if line.startswith("def")))
+    def_line = def_line.replace("test_", "test_templated_task_")
+    # print(def_line)
+    lines = lines.splitlines()
+    lines[0] = def_line
+    print(lines)
+    # print(lines)
+    break
+
+    # modified_lines = lines.replace("Task(", "TaskTemplate(")
+    # exec(modified_lines)
