fca3ff024e3a6849e45de4cef55155db948c69e8
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2022-05-07 10:16:35 +0200

    little tidying up
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

--- tests/test_task_template.py
+++ tests/test_task_template.py
@@ -3,13 +3,26 @@ from test_task import task_security_context_kwargs  # noqa
 import inspect
 import test_task
 
+from pathlib import Path
+
+
+# Get test_task.py path into Path.
+test_task_file = Path(__file__).parent.joinpath("test_task.py")
+
+# Get all test names defined in test_task.py.
+_test_func_names = [test for test in test_task.__dir__() if test.startswith("test")]
+
 
 def create_task_from_templated_task(*args, **kwargs):
+    """
+    Use this function to modify imported tests from test_task.py to use task
+    generated from task template.
+    """
     return TaskTemplate(*args, **kwargs).task()
 
 
 # Get all imports from test_task.py and import here.
-with open("tests/test_task.py", "r") as file:
+with open(test_task_file, "r") as file:
     imports = [
         line
         for line in file.readlines()
@@ -19,32 +32,31 @@ with open("tests/test_task.py", "r") as file:
         exec(i)
 
 
-# Get all test names defined in test_task.py.
-_test_func_names = [test for test in test_task.__dir__() if test.startswith("test")]
-
-# Modify all tests to use TaskTemplate.
+# Modify all tests to use TaskTemplate. Execution of Task(...) and
+# TaskTemplate(...) should be the same since TaskTemplate inherits from Task and
+# adds another function.
 for test_name in _test_func_names:
-    f = test_task.__getattribute__(test_name)
-    lines = inspect.getsource(f)
-    modified_lines = lines.replace("Task(", "TaskTemplate(")
-    exec(modified_lines)
+    test_func = test_task.__getattribute__(test_name)
+    source = inspect.getsource(test_func)
+    modified_source = source.replace("Task(", "TaskTemplate(")
+    exec(modified_source)
 
-# Modify all tests to use TaskTemplate(...).task.
+
+# Modify all tests to use TaskTemplate(...).task. And change test names to avoid
+# duplicates.
 for test_name in _test_func_names:
-    f = test_task.__getattribute__(test_name)
-    lines = inspect.getsource(f)
-    lines = lines.splitlines()
+    test_func = test_task.__getattribute__(test_name)
+    source = inspect.getsource(test_func)
+    lines = source.splitlines()
 
     for index, line in enumerate(lines):
         # Not always first line because of @pytest decorators.
         if line.startswith("def"):
             def_line = line.replace("test_", "test_templated_")
             lines[index] = def_line
-            print(def_line)
 
-    lines = "\n".join(lines)
-    lines = lines.replace("Task(", "create_task_from_templated_task(")
-    exec(lines)
-
-
-# TODO: test task with all possible tasks: python func, bash func, container, daemon, else...?
+    modified_source = "\n".join(lines)
+    modified_source = modified_source.replace(
+        "Task(", f"{create_task_from_templated_task.__name__}("
+    )
+    exec(modified_source)
