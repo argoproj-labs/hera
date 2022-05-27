08cce103386bc5f12e96e64bb449b068fe58f8d2
erauner12 <59383957+erauner12@users.noreply.github.com>
2022-05-27 06:35:19 -0500

    update ExistingVolume import (#166)

--- examples/existing_volume.py
+++ examples/existing_volume.py
@@ -2,7 +2,7 @@
 This example showcases how Hera supports mounting existing volumes. These volumes are expected to already be
 provisioned and available as a persistent volume claim in the K8S cluster where Argo runs.
 """
-from hera.existing_volume import ExistingVolume
+from hera.volumes import ExistingVolume
 from hera.resources import Resources
 from hera.task import Task
 from hera.workflow import Workflow
