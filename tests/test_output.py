c98b19869ac24016f4ffec0ff0905e9a51a81ee2
flaviuvadan <flaviuvadan@gmail.com>
2022-06-12 18:13:56 -0400

    add extra test for output param
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

--- tests/test_output.py
+++ tests/test_output.py
@@ -21,3 +21,13 @@ def test_output_parameter_returns_expected_spec():
     assert s.name == 'o'
     assert s.value_from.default == 'd'
     assert s.value_from.path == '/test.txt'
+
+    o = OutputPathParameter('o', '/test.txt')
+    assert o.name == 'o'
+    assert o.path == '/test.txt'
+    assert o.default is None
+
+    s = o.get_spec()
+    assert s.name == 'o'
+    assert not hasattr(s.value_from, 'default')
+    assert s.value_from.path == '/test.txt'
