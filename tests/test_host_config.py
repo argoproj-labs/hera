2a1bc50120889763834bd621ce7b1a0a7943ab9d
Jacob Hayes <jacob.r.hayes@gmail.com>
2022-06-13 11:36:37 -0400

    Add set_global_token_callback to handle refreshing expiring tokens

--- tests/test_host_config.py
+++ tests/test_host_config.py
@@ -16,10 +16,25 @@ def test_config_fails_when_no_host_is_provided():
     assert str(e.value) == 'A configuration/service host is required for submitting workflows'
 
 
-def test_client_uses_global_token():
+@pytest.mark.parametrize(
+    ["set_token"],
+    [
+        (lambda: set_global_token('token'),),
+        (lambda: set_global_token(lambda: 'token'),),
+    ],
+)
+def test_client_uses_global_token(set_token):
     set_global_host('http://localhost:2746')
-    set_global_token('token')
+    set_token()
     client = Client(Config())
     assert client.api_client.default_headers['Authorization'] == 'Bearer token'
     set_global_token(None)
+    set_global_host(None)
+
+
+def test_config_fails_when_no_token_is_provided():
+    set_global_host('http://localhost:2746')
+    with pytest.raises(AssertionError) as e:
+        Client(Config())
+    assert str(e.value) == 'No token was provided and no global token was found.'
     set_global_host(None)
