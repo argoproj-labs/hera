951283f171da87d36e645421fef16c1bcd0f8029
flaviuvadan <flaviuvadan@gmail.com>
2022-08-28 12:12:15 -0600

    comment on pyproject and type ignore
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

--- src/hera/_version.py
+++ src/hera/_version.py
@@ -1,3 +1,4 @@
-import pkg_resources
+# it would be ideal to have all types but pointless to bring in another package for such a brief usage
+import pkg_resources  # type: ignore
 
 version = pkg_resources.get_distribution("hera-workflows").version
