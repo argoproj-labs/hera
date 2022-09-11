aacd54ad2cc4d1a8b436ab33bea32cf20f214112
flaviuvadan <flaviuvadan@gmail.com>
2022-09-11 17:18:21 -0600

    document DAG context
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

--- src/hera/_context.py
+++ src/hera/_context.py
@@ -6,24 +6,31 @@ from hera.task import Task
 
 
 class _DAG_context(threading.local):
+    """Holds the directed acyclic graph context of a Hera workflow"""
+
     def __init__(self) -> None:
         super().__init__()
-        self._dag: List[DAG] = []
+        self._dags: List[DAG] = []
 
     def enter(self, d: DAG) -> None:
-        self._dag.append(d)
+        """Inject a DAG into the overall Hera DAG context"""
+        self._dags.append(d)
 
     def exit(self) -> None:
-        self._dag.pop()
+        """Eject a DAG off the overall Hera DAG context"""
+        self._dags.pop()
 
     def is_set(self) -> bool:
-        return self._dag != []
+        """Return whether there are any DAGs set on the Hera DAG context"""
+        return self._dags != []
 
     def add_task(self, t: Task) -> None:
-        self._dag[-1].add_task(t)
+        """Add a task to the DAG that was added last to the context"""
+        self._dags[-1].add_task(t)
 
     def add_tasks(self, *ts: Task) -> None:
-        self._dag[-1].add_tasks(*ts)
+        """Adds a collection of tasks to the DAG that was added last to the context"""
+        self._dags[-1].add_tasks(*ts)
 
 
 dag_context = _DAG_context()
