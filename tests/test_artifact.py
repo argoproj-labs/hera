d6059fe7e55e63a95000da5273c74445644cffb1
flaviuvadan <flaviuvadan@gmail.com>
2022-09-25 15:49:17 -0600

    add image and artifact tests
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

--- tests/test_artifact.py
+++ tests/test_artifact.py
@@ -1,69 +1,158 @@
 from argo_workflows.models import (
+    IoArgoprojWorkflowV1alpha1ArchiveStrategy,
     IoArgoprojWorkflowV1alpha1Artifact,
+    IoArgoprojWorkflowV1alpha1GCSArtifact,
     IoArgoprojWorkflowV1alpha1GitArtifact,
     IoArgoprojWorkflowV1alpha1HTTPArtifact,
+    IoArgoprojWorkflowV1alpha1S3Artifact,
 )
 
-from hera import Artifact, GitArtifact, HttpArtifact, Task
+from hera import Artifact, GCSArtifact, GitArtifact, HttpArtifact, S3Artifact, Task
 
 
-def test_output_artifact_contains_expected_fields():
-    name = "test-artifact"
-    path = "/input/path"
-    from_task = "produce-artifact"
+class TestArtifact:
+    def test_artifact_as_argument_returns_expected_artifact(self):
+        artifact = Artifact("a", from_task="b", path="/a", sub_path="/b/c")
+        argument = artifact.as_argument()
+        assert isinstance(argument, IoArgoprojWorkflowV1alpha1Artifact)
+        assert hasattr(argument, "_from")
+        assert argument._from == "b"
+        assert hasattr(argument, "sub_path")
+        assert argument.sub_path == "/b/c"
 
-    expected = Artifact(name=name, path=path, from_task=f"{{{{tasks.{from_task}.outputs.artifacts.{name}}}}}")
-    t1 = Task("produce-artifact", outputs=[Artifact(name, path)])
-    actual = t1.get_artifact(name)
+    def test_output_artifact_contains_expected_fields(self):
+        name = "test-artifact"
+        path = "/input/path"
+        from_task = "produce-artifact"
 
-    assert isinstance(actual, Artifact)
-    assert actual.from_task == expected.from_task
-    assert actual.name == expected.name
-    assert actual.path == expected.path
+        expected = Artifact(name=name, path=path, from_task=f"{{{{tasks.{from_task}.outputs.artifacts.{name}}}}}")
+        t1 = Task("produce-artifact", outputs=[Artifact(name, path)])
+        actual = t1.get_artifact(name)
 
-    new_path = "/input/alternative_path"
-    expected = Artifact(name=name, path=new_path, from_task=f"{{{{tasks.{from_task}.outputs.artifacts.{name}}}}}")
-    actual = t1.get_artifact(name).to_path(new_path)
-    assert isinstance(actual, Artifact)
-    assert actual.from_task == expected.from_task
-    assert actual.name == expected.name
-    assert actual.path == expected.path
+        assert isinstance(actual, Artifact)
+        assert actual.from_task == expected.from_task
+        assert actual.name == expected.name
+        assert actual.path == expected.path
 
-    new_name = "test-artifact2"
-    expected = Artifact(name=new_name, path=new_path, from_task=f"{{{{tasks.{from_task}.outputs.artifacts.{name}}}}}")
-    actual = t1.get_artifact(name).to_path(new_path).as_name(new_name)
-    assert isinstance(actual, Artifact)
-    assert actual.from_task == expected.from_task
-    assert actual.name == expected.name
-    assert actual.path == expected.path
+        new_path = "/input/alternative_path"
+        expected = Artifact(name=name, path=new_path, from_task=f"{{{{tasks.{from_task}.outputs.artifacts.{name}}}}}")
+        actual = t1.get_artifact(name).to_path(new_path)
+        assert isinstance(actual, Artifact)
+        assert actual.from_task == expected.from_task
+        assert actual.name == expected.name
+        assert actual.path == expected.path
 
-
-def test_git_artifact():
-    name = "git-artifact"
-    path = "/src"
-    repo = "https://github.com/awesome/awesome-repo.git"
-    revision = "main"
-
-    expected = IoArgoprojWorkflowV1alpha1Artifact(
-        name=name, path=path, git=IoArgoprojWorkflowV1alpha1GitArtifact(repo=repo, revision=revision)
-    )
-    actual = GitArtifact(name, path, repo, revision).as_argument()
-    actual_input = GitArtifact(name, path, repo, revision).as_input()
-
-    assert actual == expected
-    assert actual_input == expected
+        new_name = "test-artifact2"
+        expected = Artifact(
+            name=new_name, path=new_path, from_task=f"{{{{tasks.{from_task}.outputs.artifacts.{name}}}}}"
+        )
+        actual = t1.get_artifact(name).to_path(new_path).as_name(new_name)
+        assert isinstance(actual, Artifact)
+        assert actual.from_task == expected.from_task
+        assert actual.name == expected.name
+        assert actual.path == expected.path
 
 
-def test_http_artifact():
-    name = "http-artifact"
-    path = "/src"
-    url = "whatever.com/my-file.txt"
+class TestS3Artifact:
+    def test_as_argument_returns_expected_argument(self):
+        artifact = S3Artifact("a", "s3://test", "test", "test", archive={"zip": "test"})
+        argument = artifact.as_argument()
+        assert isinstance(argument, IoArgoprojWorkflowV1alpha1Artifact)
+        assert hasattr(argument, "name")
+        assert argument.name == "a"
+        assert hasattr(argument, "path")
+        assert argument.path == "s3://test"
+        assert hasattr(argument, "s3")
+        assert isinstance(argument.s3, IoArgoprojWorkflowV1alpha1S3Artifact)
+        assert argument.s3.bucket == "test"
+        assert argument.s3.key == "test"
+        assert hasattr(argument, "archive")
+        assert isinstance(argument.archive, IoArgoprojWorkflowV1alpha1ArchiveStrategy)
+        assert hasattr(argument.archive, "zip")
+        assert argument.archive.zip == "test"
 
-    expected = IoArgoprojWorkflowV1alpha1Artifact(
-        name=name, path=path, http=IoArgoprojWorkflowV1alpha1HTTPArtifact(url=url)
-    )
-    actual = HttpArtifact(name, path, url).as_argument()
-    actual_input = HttpArtifact(name, path, url).as_input()
+    def test_as_output_returns_expected_output(self):
+        artifact = S3Artifact("a", "s3://test", "test", "test", archive={"zip": "test"})
+        output = artifact.as_output()
+        assert isinstance(output, IoArgoprojWorkflowV1alpha1Artifact)
+        assert hasattr(output, "name")
+        assert output.name == "a"
+        assert hasattr(output, "path")
+        assert output.path == "s3://test"
+        assert hasattr(output, "s3")
+        assert isinstance(output.s3, IoArgoprojWorkflowV1alpha1S3Artifact)
+        assert output.s3.bucket == "test"
+        assert output.s3.key == "test"
+        assert hasattr(output, "archive")
+        assert isinstance(output.archive, IoArgoprojWorkflowV1alpha1ArchiveStrategy)
+        assert hasattr(output.archive, "zip")
+        assert output.archive.zip == "test"
 
-    assert actual == expected
-    assert actual_input == expected
+
+class TestGCSArtifact:
+    def test_as_argument_returns_expected_argument(self):
+        artifact = GCSArtifact("a", "gs://test", "test", "test", archive={"zip": "test"})
+        argument = artifact.as_argument()
+        assert isinstance(argument, IoArgoprojWorkflowV1alpha1Artifact)
+        assert hasattr(argument, "name")
+        assert argument.name == "a"
+        assert hasattr(argument, "path")
+        assert argument.path == "gs://test"
+        assert hasattr(argument, "gcs")
+        assert isinstance(argument.gcs, IoArgoprojWorkflowV1alpha1GCSArtifact)
+        assert argument.gcs.bucket == "test"
+        assert argument.gcs.key == "test"
+        assert hasattr(argument, "archive")
+        assert isinstance(argument.archive, IoArgoprojWorkflowV1alpha1ArchiveStrategy)
+        assert hasattr(argument.archive, "zip")
+        assert argument.archive.zip == "test"
+
+    def test_as_output_returns_expected_output(self):
+        artifact = GCSArtifact("a", "gs://test", "test", "test", archive={"zip": "test"})
+        output = artifact.as_output()
+        assert isinstance(output, IoArgoprojWorkflowV1alpha1Artifact)
+        assert hasattr(output, "name")
+        assert output.name == "a"
+        assert hasattr(output, "path")
+        assert output.path == "gs://test"
+        assert hasattr(output, "gcs")
+        assert isinstance(output.gcs, IoArgoprojWorkflowV1alpha1GCSArtifact)
+        assert output.gcs.bucket == "test"
+        assert output.gcs.key == "test"
+        assert hasattr(output, "archive")
+        assert isinstance(output.archive, IoArgoprojWorkflowV1alpha1ArchiveStrategy)
+        assert hasattr(output.archive, "zip")
+        assert output.archive.zip == "test"
+
+
+class TestGitArtifact:
+    def test_git_artifact_returns_expected_output(self):
+        name = "git-artifact"
+        path = "/src"
+        repo = "https://github.com/awesome/awesome-repo.git"
+        revision = "main"
+
+        expected = IoArgoprojWorkflowV1alpha1Artifact(
+            name=name, path=path, git=IoArgoprojWorkflowV1alpha1GitArtifact(repo=repo, revision=revision)
+        )
+        actual = GitArtifact(name, path, repo, revision).as_argument()
+        actual_input = GitArtifact(name, path, repo, revision).as_input()
+
+        assert actual == expected
+        assert actual_input == expected
+
+
+class TestHTTPArtifact:
+    def test_http_artifact_returns_expected_output(self):
+        name = "http-artifact"
+        path = "/src"
+        url = "whatever.com/my-file.txt"
+
+        expected = IoArgoprojWorkflowV1alpha1Artifact(
+            name=name, path=path, http=IoArgoprojWorkflowV1alpha1HTTPArtifact(url=url)
+        )
+        actual = HttpArtifact(name, path, url).as_argument()
+        actual_input = HttpArtifact(name, path, url).as_input()
+
+        assert actual == expected
+        assert actual_input == expected
--- tests/test_image.py
+++ tests/test_image.py
@@ -2,7 +2,6 @@ from hera import ImagePullPolicy
 
 
 class TestImagePullPolicy:
-
     def test_str_returns_expected_value(self):
         assert str(ImagePullPolicy.Always) == "Always"
         assert str(ImagePullPolicy.Never) == "Never"
