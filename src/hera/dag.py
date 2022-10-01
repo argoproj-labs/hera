f4aa995330f7018ff9c28038e85a4d867d526c4c
flaviuvadan <flaviuvadan@gmail.com>
2022-10-01 08:39:51 -0600

    add more return types in dag
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

--- src/hera/dag.py
+++ src/hera/dag.py
@@ -5,6 +5,7 @@ from argo_workflows.models import (
     IoArgoprojWorkflowV1alpha1DAGTemplate,
     IoArgoprojWorkflowV1alpha1Template,
     PersistentVolumeClaim,
+    IoArgoprojWorkflowV1alpha1DAGTask,
 )
 from argo_workflows.models import Volume as ArgoVolume
 
@@ -45,7 +46,7 @@ class DAG(IO):
         self.outputs = outputs or []
         self.tasks: List[Task] = []
 
-    def _build_templates(self):
+    def _build_templates(self) -> Optional[List[IoArgoprojWorkflowV1alpha1Template]]:
         """Assembles the templates from sub-DAGs of the DAG"""
         templates = [t for t in [t._build_template() for t in self.tasks] if t]
         # Assemble the templates from sub-dags
@@ -53,7 +54,7 @@ class DAG(IO):
         sub_templates = [t for sublist in sub_templates for t in sublist]  # flatten
         return templates + sub_templates
 
-    def _build_dag_tasks(self):
+    def _build_dag_tasks(self) -> Optional[List[IoArgoprojWorkflowV1alpha1DAGTask]]:
         """Assembles all the DAG tasks"""
         return [t for t in [t._build_dag_task() for t in self.tasks if not t.is_exit_task] if t]
 
@@ -70,24 +71,23 @@ class DAG(IO):
         for volume_claims in sub_volume_claims:
             for v in volume_claims:
                 vcs[v.metadata.name] = v
-
-        return [v for _, v in vcs.items()]
+        return list(vcs.values())
 
     def _build_persistent_volume_claims(self) -> List[ArgoVolume]:
         """Assembles the persistent volume claim templates"""
         # Make sure we only have unique names
-        pcvs = dict()
+        pvcs = dict()
         for t in self.tasks:
             for v in t._build_persistent_volume_claims():
-                pcvs[v.name] = v
+                pvcs[v.name] = v
 
         # sub-claims:
         sub_volume_claims = [t.dag._build_persistent_volume_claims() for t in self.tasks if t.dag]
         for volume_claims in sub_volume_claims:
             for v in volume_claims:
-                pcvs[v.name] = v
+                pvcs[v.name] = v
 
-        return [v for _, v in pcvs.items()]
+        return list(pvcs.values())
 
     def build(self) -> List[IoArgoprojWorkflowV1alpha1Template]:
         """Assembles the main DAG/workflow template"""
@@ -120,19 +120,21 @@ class DAG(IO):
         self.in_context = False
         dag_context.exit()
 
-    def add_task(self, t: Task) -> None:
+    def add_task(self, t: Task) -> "DAG":
         """Add a task to the DAG. Note that tasks are added automatically when the DAG context is used"""
         add_task(self, t)
+        return self
 
-    def add_tasks(self, *ts: Task) -> None:
+    def add_tasks(self, *ts: Task) -> "DAG":
         """Add a collection of tasks to the DAG.
 
         Note that tasks are added automatically when the DAG context is used
         """
         add_tasks(self, *ts)
+        return self
 
-    def get_parameter(self, name: str):
-        """Returns the parameter specification of a DAG"""
-        if next((p for p in self.inputs if p.name == name), None) is None:
-            raise KeyError("`{name}` not in DAG inputs")
+    def get_parameter(self, name: str) -> Parameter:
+        """Returns a DAG output as a `Parameter` to use an input somewhere else"""
+        if next((p for p in self.outputs if p.name == name), None) is None:
+            raise KeyError(f'Could not assemble a parameter as `{name}` is not a DAG output')
         return Parameter(name, value=f"{{{{inputs.parameters.{name}}}}}")
