58b8524b63b6d3b4c57fccf94014b7beaffce2c8
Flaviu Vadan <flaviuvadan@gmail.com>
2022-10-01 10:55:17 -0600

    Expand coverage of files missing small coverage percentages (#291)
    * add retry policy test
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    
    * expand host config tests
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    
    * expand validator tests
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    
    * format
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

---
+++ tests/test_retry_policy.py
@@ -0,0 +1,10 @@
+from hera.retry_policy import RetryPolicy
+
+
+class TestRetryPolicy:
+    def test_str_returns_expected_value(self):
+        assert str(RetryPolicy.Always) == "Always"
+        assert str(RetryPolicy.OnFailure) == "OnFailure"
+        assert str(RetryPolicy.OnError) == "OnError"
+        assert str(RetryPolicy.OnTransientError) == "OnTransientError"
+        assert len(RetryPolicy) == 4
