629113301fb5ca75157f7b605953510b57745b1f
flaviuvadan <flaviuvadan@gmail.com>
2022-10-28 15:19:58 -0600

    add lint example
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

---
+++ examples/lint.py
@@ -0,0 +1,23 @@
+"""This example showcases how to lint workflows supported by Hera"""
+
+from hera import CronWorkflow, Workflow, WorkflowTemplate, Task
+
+
+def say(msg: str) -> None:
+    print(msg)
+
+
+with Workflow('test-w') as w:
+    Task('say', say, with_param=['Hello, world!'])
+
+w.lint()
+
+with CronWorkflow('cron-test-w', '* * * * *') as cw:
+    Task('say', say, with_param=['Hello, world!'])
+
+cw.lint()
+
+with WorkflowTemplate('test-wt', labels={'a': 'b'}) as wt:
+    Task('say', say, with_param=['Hello, world!'])
+
+wt.lint()
