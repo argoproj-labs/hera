96ba813449d9e2c60cb541ad534d5c88556a5d83
Jacob Hayes <jacob.r.hayes@gmail.com>
2022-11-08 19:33:30 -0500

    Fix WorkflowService.get_workflow_link when using set_global_host (#350)
    Signed-off-by: Jacob Hayes <jacob.r.hayes@gmail.com>
    Signed-off-by: Flaviu Vadan <flaviuvadan@gmail.com>

--- tests/test_workflow_service.py
+++ tests/test_workflow_service.py
@@ -8,8 +8,8 @@ from hera import WorkflowService
 def test_ws_has_expected_fields_upon_init():
     ws = WorkflowService(host="https://abc.com", token="abc", verify_ssl=True, namespace="argo")
 
-    assert ws._host == "https://abc.com"
-    assert ws._verify_ssl
+    assert ws._config.host == "https://abc.com"
+    assert ws._config.verify_ssl
     assert ws._namespace == "argo"
     assert isinstance(ws._api_client, ApiClient)
 
