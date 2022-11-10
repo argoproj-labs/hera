5f985ca50b633abb6d81185409d91fbf51e476cf
flaviuvadan <flaviuvadan@gmail.com>
2022-11-10 14:15:54 -0700

    add test for independent hook addition to list, and use both list/tuple for hook setting test
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

--- tests/test_global_config.py
+++ tests/test_global_config.py
@@ -68,8 +68,11 @@ class TestGlobalConfig:
         c.task_post_init_hooks = [hook1, hook2]
         assert len(c.task_post_init_hooks) == 2
 
-        c.task_post_init_hooks = [hook1, hook2]
+        c.task_post_init_hooks = (hook1, hook2)
         assert len(c.task_post_init_hooks) == 4
+
+        c.task_post_init_hooks = hook1
+        assert len(c.task_post_init_hooks) == 5
         c.reset()
 
     def test_workflow_post_init_hooks(self):
@@ -85,6 +88,9 @@ class TestGlobalConfig:
         c.workflow_post_init_hooks = [hook1, hook2]
         assert len(c.workflow_post_init_hooks) == 2
 
-        c.workflow_post_init_hooks = [hook1, hook2]
+        c.workflow_post_init_hooks = (hook1, hook2)
         assert len(c.workflow_post_init_hooks) == 4
+
+        c.workflow_post_init_hooks = hook1
+        assert len(c.workflow_post_init_hooks) == 5
         c.reset()
