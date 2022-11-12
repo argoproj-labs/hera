9552d5a867e45ca2e2b35c96f075f365f11d0500
Jacob Hayes <jacob.r.hayes@gmail.com>
2022-11-11 21:14:16 -0500

    Remove unused config reset and imports in tests
    Signed-off-by: Jacob Hayes <jacob.r.hayes@gmail.com>

--- tests/test_global_config.py
+++ tests/test_global_config.py
@@ -9,7 +9,6 @@ class TestGlobalConfig:
         assert c.host is None
         c.host = '123'
         assert c.host == '123'
-        c.reset()
 
     def test_token(self):
         c = _GlobalConfig()
@@ -18,42 +17,36 @@ class TestGlobalConfig:
         assert c.token == '123'
         c.token = lambda: '123'
         assert c.token == '123'
-        c.reset()
 
     def test_verify_ssl(self):
         c = _GlobalConfig()
         assert c.verify_ssl
         c.verify_ssl = False
         assert not c.verify_ssl
-        c.reset()
 
     def test_api_version(self):
         c = _GlobalConfig()
         assert c.api_version == "argoproj.io/v1alpha1"
         c.api_version = "123"
         assert c.api_version == "123"
-        c.reset()
 
     def test_namespace(self):
         c = _GlobalConfig()
         assert c.namespace == "default"
         c.namespace = "123"
         assert c.namespace == "123"
-        c.reset()
 
     def test_image(self):
         c = _GlobalConfig()
         assert c.image == "python:3.7"
         c.image = "123"
         assert c.image == "123"
-        c.reset()
 
     def test_service_account_name(self):
         c = _GlobalConfig()
         assert c.service_account_name is None
         c.service_account_name = "123"
         assert c.service_account_name == "123"
-        c.reset()
 
     def test_task_post_init_hooks(self):
         c = _GlobalConfig()
@@ -71,8 +64,6 @@ class TestGlobalConfig:
         c.task_post_init_hooks = (hook1, hook2)
         assert len(c.task_post_init_hooks) == 2
 
-        c.reset()
-
     def test_workflow_post_init_hooks(self):
         c = _GlobalConfig()
         assert c.workflow_post_init_hooks == ()
@@ -88,4 +79,3 @@ class TestGlobalConfig:
 
         c.workflow_post_init_hooks = (hook1, hook2)
         assert len(c.workflow_post_init_hooks) == 2
-        c.reset()
