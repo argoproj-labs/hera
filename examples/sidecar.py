bebdfcdce6950759259098797a3de94087d6bdcd
flaviuvadan <flaviuvadan@gmail.com>
2022-11-28 18:31:25 -0700

    fix lint
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

--- examples/sidecar.py
+++ examples/sidecar.py
@@ -9,7 +9,9 @@ with Workflow("sidecar-", generate_name=True) as w:
         image="alpine:latest",
         command=["sh", "-c"],
         args=[
-            "apk update && apk add curl && until curl -XPOST 'http://127.0.0.1:8086/query' --data-urlencode 'q=CREATE DATABASE mydb' ; do sleep .5; done && for i in $(seq 1 20); do curl -XPOST 'http://127.0.0.1:8086/write?db=mydb' -d \"cpu,host=server01,region=uswest load=$i\" ; sleep .5 ; done"
+            "apk update && apk add curl && until curl -XPOST 'http://127.0.0.1:8086/query' --data-urlencode "
+            "'q=CREATE DATABASE mydb' ; do sleep .5; done && for i in $(seq 1 20); do curl -XPOST "
+            "'http://127.0.0.1:8086/write?db=mydb' -d \"cpu,host=server01,region=uswest load=$i\" ; sleep .5 ; done"
         ],
         sidecars=[Sidecar("influxdb", image="influxdb:1.2", command=["influxd"])],
     )
