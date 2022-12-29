8dc2ec4ace6a4da65f8610e94783aac21608dafa
flaviuvadan <flaviuvadan@gmail.com>
2022-12-29 15:03:33 -0700

    add base model
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

---
+++ src/hera/_base_model.py
@@ -0,0 +1,26 @@
+from types import ModuleType
+from typing import Optional
+
+from pydantic import BaseModel
+
+_yaml: Optional[ModuleType] = None
+try:
+    import yaml
+
+    _yaml = yaml
+except ImportError:
+    _yaml = None
+
+
+class ArgoBaseModel(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+
+    def to_yaml(self, **yaml_kwargs) -> str:
+        """Returns a YAML representation of the object"""
+        if _yaml is None:
+            raise ImportError(
+                "Attempted to use `to_yaml` but PyYAML is not available. "
+                "Install `hera-workflows[yaml]` to install the extra dependency"
+            )
+        return _yaml.dump(self.dict(exclude_none=True, by_alias=True), **yaml_kwargs)
