133b4f847f2e373ce24ce603d8314ab37dad1aa5
flaviuvadan <flaviuvadan@gmail.com>
2023-01-07 20:20:54 -0500

    add workflow template operations
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

--- src/hera/workflow_template.py
+++ src/hera/workflow_template.py
@@ -1,9 +1,15 @@
 """The implementation of a Hera cron workflow for Argo-based cron workflows"""
-from typing import Tuple
-
-from argo_workflows.models import IoArgoprojWorkflowV1alpha1WorkflowTemplate
+from typing import Optional
 
 from hera.global_config import GlobalConfig
+from hera.models import CreateOptions
+from hera.models import WorkflowTemplate as _ModelWorkflowTemplate
+from hera.models import (
+    WorkflowTemplateCreateRequest,
+    WorkflowTemplateDeleteResponse,
+    WorkflowTemplateLintRequest,
+    WorkflowTemplateUpdateRequest,
+)
 from hera.workflow import Workflow
 
 
@@ -17,33 +23,70 @@ class WorkflowTemplate(Workflow):
     See: https://argoproj.github.io/argo-workflows/workflow-templates/
     """
 
-    def build(self) -> IoArgoprojWorkflowV1alpha1WorkflowTemplate:
+    def build(self) -> _ModelWorkflowTemplate:
         """Builds the workflow"""
         spec = super()._build_spec()
-        return IoArgoprojWorkflowV1alpha1WorkflowTemplate(
+        return _ModelWorkflowTemplate(
             api_version=GlobalConfig.api_version,
             kind=self.__class__.__name__,
             metadata=self._build_metadata(),
             spec=spec,
         )
 
-    def create(self) -> "WorkflowTemplate":
+    def create(
+        self, namespace: str = GlobalConfig.namespace, create_options: Optional[CreateOptions] = None
+    ) -> "WorkflowTemplate":
         """Creates a workflow template"""
         if self.in_context:
             raise ValueError("Cannot invoke `create` when using a Hera context")
-        self.service.create_workflow_template(self.build())
-        return self
+        return self.service.create_workflow_template(
+            namespace,
+            WorkflowTemplateCreateRequest(create_options=create_options, namespace=namespace, template=self.build()),
+        )
 
-    def lint(self) -> "WorkflowTemplate":
+    def lint(
+        self, namespace: str = GlobalConfig.namespace, create_options: Optional[CreateOptions] = None
+    ) -> "WorkflowTemplate":
         """Lint the workflow"""
-        self.service.lint_workflow_template(self.build())
-        return self
+        return self.service.lint_workflow_template(
+            namespace,
+            WorkflowTemplateLintRequest(
+                create_options=create_options,
+                namespace=namespace,
+                template=self.build(),
+            ),
+        )
 
-    def update(self) -> "WorkflowTemplate":
+    def update(self, namespace: str = GlobalConfig.namespace) -> "WorkflowTemplate":
         """Updates an existing workflow template"""
-        self.service.update_workflow_template(self.name, self.build())
-        return self
+        return self.service.update_workflow_template(
+            namespace,
+            self.name,
+            WorkflowTemplateUpdateRequest(
+                name=self.name,
+                namespace=namespace,
+                template=self.build(),
+            ),
+        )
 
-    def delete(self) -> Tuple[object, int, dict]:
+    def delete(
+        self,
+        namespace: str = GlobalConfig.namespace,
+        grace_period_seconds: Optional[str] = None,
+        uid: Optional[str] = None,
+        resource_version: Optional[str] = None,
+        orphan_dependents: Optional[bool] = None,
+        propagation_policy: Optional[str] = None,
+        dry_run: Optional[list] = None,
+    ) -> WorkflowTemplateDeleteResponse:
         """Deletes the workflow"""
-        return self.service.delete_workflow_template(self.name)
+        return self.service.delete_workflow_template(
+            namespace,
+            self.name,
+            grace_period_seconds=grace_period_seconds,
+            uid=uid,
+            resource_version=resource_version,
+            orphan_dependents=orphan_dependents,
+            propagation_policy=propagation_policy,
+            dry_run=dry_run,
+        )
