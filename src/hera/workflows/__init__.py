f4b52318ab0ded70dfb0c6fe00610b51fe79e0a9
flaviuvadan <flaviuvadan@gmail.com>
2023-02-06 15:34:19 -0700

    add workflows to project structure
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

---
+++ src/hera/workflows/__init__.py
@@ -0,0 +1,81 @@
+from hera.workflows._context import dag_context
+from hera.workflows.action import (
+    ExecAction,
+    GRPCAction,
+    HTTPGetAction,
+    HTTPHeader,
+    Scheme,
+    TCPSocketAction,
+)
+from hera.workflows.affinity import (
+    Affinity,
+    Expression,
+    Field,
+    LabelOperator,
+    LabelSelector,
+    LabelSelectorRequirement,
+    NodeAffinity,
+    NodeSelector,
+    NodeSelectorRequirement,
+    NodeSelectorTerm,
+    PodAffinity,
+    PodAffinityTerm,
+    PodAntiAffinity,
+    PreferredSchedulingTerm,
+    WeightedPodAffinityTerm,
+)
+from hera.workflows.archive import Archive
+from hera.workflows.artifact import (
+    Artifact,
+    GCSArtifact,
+    GitArtifact,
+    HttpArtifact,
+    RawArtifact,
+    S3Artifact,
+)
+from hera.workflows.backoff import Backoff
+from hera.workflows.client import Client
+from hera.workflows.config import Config
+from hera.workflows.cron_workflow import ConcurrencyPolicy, CronWorkflow
+from hera.workflows.dag import DAG
+from hera.workflows.env import ConfigMapEnv, Env, FieldEnv, SecretEnv
+from hera.workflows.env_from import ConfigMapEnvFrom, SecretEnvFrom
+from hera.workflows.global_config import GlobalConfig
+from hera.workflows.host_alias import HostAlias
+
+# the following host configurations are deprecated. See `GlobalConfig` instead
+from hera.workflows.image import ImagePullPolicy
+from hera.workflows.lifecycle import Lifecycle, LifecycleHandler
+from hera.workflows.memoize import Memoize
+from hera.workflows.operator import Operator
+from hera.workflows.parameter import Parameter
+from hera.workflows.port import ContainerPort, Protocol
+from hera.workflows.probe import Probe
+from hera.workflows.resource_template import ResourceTemplate
+from hera.workflows.resources import Resources
+from hera.workflows.retry_policy import RetryPolicy
+from hera.workflows.retry_strategy import RetryStrategy
+from hera.workflows.security_context import TaskSecurityContext, WorkflowSecurityContext
+from hera.workflows.sequence import Sequence
+from hera.workflows.sidecar import Sidecar
+from hera.workflows.suspend import Suspend
+from hera.workflows.task import Task, TaskResult
+from hera.workflows.template_ref import TemplateRef
+from hera.workflows.toleration import GPUToleration, Toleration
+from hera.workflows.ttl_strategy import TTLStrategy
+from hera.workflows.value_from import ValueFrom
+from hera.workflows.volume_claim_gc import VolumeClaimGCStrategy
+from hera.workflows.volumes import (
+    AccessMode,
+    ConfigMapVolume,
+    EmptyDirVolume,
+    ExistingVolume,
+    SecretVolume,
+    Volume,
+    VolumeDevice,
+    VolumeMount,
+)
+from hera.workflows.workflow import Workflow
+from hera.workflows.workflow_service import WorkflowService
+from hera.workflows.workflow_status import WorkflowStatus
+from hera.workflows.workflow_template import WorkflowTemplate
