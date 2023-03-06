3df47dac08747f7d33cfa613498e58b8eb11f8fa
flaviuvadan <flaviuvadan@gmail.com>
2023-03-06 14:30:42 -0500

    Add metaclass to events
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>

--- src/hera/workflows/v5/_meta.py
+++
@@ -1,7 +0,0 @@
-from pydantic.main import ModelMetaclass as _PyModelMetaclass
-
-
-class ModelMetaclass(_PyModelMetaclass):
-    def __call__(cls, *args, **kwargs):
-        obj = type.__call__(cls, *args, **kwargs)
-        return obj.__post_init__()
