8a4a4a69c7535d9a97ea4b517c24d4f8b62a57fc
Flaviu Vadan <flaviuvadan@gmail.com>
2023-03-08 16:37:04 -0500

    Add Data and HTTP templates (#454)
    * add HTTP template and add missing call in suspend
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    
    * add http
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    
    * add data template
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    
    * remove call from http
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    
    * remove call from data
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    
    * format
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    
    * Update gitpod configuration
    
    Signed-off-by: Sambhav Kothari <sambhavs.email@gmail.com>
    
    * Add data tests
    
    Signed-off-by: Sambhav Kothari <sambhavs.email@gmail.com>
    
    * Add HTTP example
    
    Signed-off-by: Sambhav Kothari <sambhavs.email@gmail.com>
    
    ---------
    
    Signed-off-by: flaviuvadan <flaviuvadan@gmail.com>
    Signed-off-by: Sambhav Kothari <sambhavs.email@gmail.com>
    Co-authored-by: Sambhav Kothari <sambhavs.email@gmail.com>

---
+++ src/hera/workflows/v5/http.py
@@ -0,0 +1,62 @@
+from typing import List, Optional
+
+from hera.workflows.models import (
+    HTTP as _ModelHTTP,
+    HTTPBodySource,
+    Template as _ModelTemplate,
+    V1HTTPHeader as HTTPHeader,
+)
+from hera.workflows.v5._mixins import _IOMixin, _SubNodeMixin, _TemplateMixin
+
+
+class HTTP(_TemplateMixin, _SubNodeMixin, _IOMixin):
+    url: str
+    body: Optional[str] = None
+    body_from: Optional[HTTPBodySource] = None
+    headers: Optional[List[HTTPHeader]] = None
+    insecure_skip_verify: Optional[bool] = None
+    method: Optional[str] = None
+    success_condition: Optional[str] = None
+    timeout_seconds: Optional[int] = None
+
+    def _build_http_template(self) -> _ModelHTTP:
+        return _ModelHTTP(
+            url=self.url,
+            body=self.body,
+            body_from=self.body_from,
+            headers=self.headers,
+            insecure_skip_verify=self.insecure_skip_verify,
+            method=self.method,
+            success_condition=self.success_condition,
+            timeout_seconds=self.timeout_seconds,
+        )
+
+    def _build_template(self) -> _ModelTemplate:
+        return _ModelTemplate(
+            active_deadline_seconds=self.active_deadline_seconds,
+            affinity=self.affinity,
+            archive_location=self.archive_location,
+            automount_service_account_token=self.automount_service_account_token,
+            executor=self.executor,
+            fail_fast=self.fail_fast,
+            host_aliases=self.host_aliases,
+            http=self._build_http_template(),
+            init_containers=self.init_containers,
+            memoize=self.memoize,
+            metadata=self._build_metadata(),
+            inputs=self._build_inputs(),
+            outputs=self._build_outputs(),
+            name=self.name,
+            node_selector=self.node_selector,
+            plugin=self.plugin,
+            priority=self.priority,
+            priority_class_name=self.priority_class_name,
+            retry_strategy=self.retry_strategy,
+            scheduler_name=self.scheduler_name,
+            security_context=self.pod_security_context,
+            service_account_name=self.service_account_name,
+            sidecars=self.sidecars,
+            synchronization=self.synchronization,
+            timeout=self.timeout,
+            tolerations=self.tolerations,
+        )
