7716ccffb626c0beef890073ef48dd7eb7bd819e
Elliot Gunton <egunton@bloomberg.net>
2023-03-09 10:47:51 +0000

    Add `with Steps`
    * Added steps.py example
    * Generated steps.yaml/upstream.steps.yaml example to track parity
    
    Signed-off-by: Elliot Gunton <egunton@bloomberg.net>

---
+++ src/hera/workflows/v5/examples/upstream/steps.py
@@ -0,0 +1,38 @@
+from hera.workflows.models import Parameter
+from hera.workflows.v5.container import Container
+from hera.workflows.v5.steps import Steps  # , Step, ParallelStep
+from hera.workflows.v5.workflow import Workflow
+
+whalesay = Container(
+    name="whalesay",
+    inputs=[Parameter(name="message")],
+    image="docker/whalesay",
+    command=["cowsay"],
+    args=["{{inputs.parameters.message}}"],
+)
+
+with Workflow(
+    generate_name="steps-",
+    entrypoint="hello-hello-hello",
+) as w:
+    with Steps(name="hello-hello-hello"):
+        pass
+    #     Step(
+    #         name="hello1",
+    #         template=whalesay,
+    #         arguments=[Parameter(name="message", value="hello1")],
+    #     )
+
+    #     with ParallelSteps():
+    #         Step(
+    #             name="hello2a",
+    #             template=whalesay,
+    #             arguments=[Parameter(name="message", value="hello2a")],
+    #         )
+    #         Step(
+    #             name="hello2b",
+    #             template=whalesay,
+    #             arguments=[Parameter(name="message", value="hello2b")],
+    #         )
+
+    w.templates.append(whalesay)
