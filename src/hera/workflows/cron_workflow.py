94bfd25fa4b2f74f321169661033c9b4e1665961
Elliot Gunton <egunton@bloomberg.net>
2023-03-15 13:45:56 +0000

    Add WorkflowTemplate create/lint targets
    Signed-off-by: GitHub <noreply@github.com>

--- src/hera/workflows/cron_workflow.py
+++ src/hera/workflows/cron_workflow.py
@@ -18,7 +18,7 @@ from hera.workflows.models import (
 )
 from hera.workflows.protocol import Templatable
 from hera.workflows.service import WorkflowsService
-from hera.workflows.workflow_spec import WorkflowSpec
+# from hera.workflows.workflow_spec import WorkflowSpec
 
 _yaml: Optional[ModuleType] = None
 try:
@@ -42,7 +42,7 @@ class CronWorkflow(ContextMixin):
     suspend: Optional[bool] = None
     timezone: Optional[str] = None
     status: Optional[CronWorkflowStatus] = None
-    workflow_spec: WorkflowSpec = WorkflowSpec()
+    # workflow_spec: WorkflowSpec = WorkflowSpec()
     workflows_service: Optional[WorkflowsService] = None
 
     @validator("workflows_service", pre=True, always=True)
@@ -63,40 +63,41 @@ class CronWorkflow(ContextMixin):
         self.workflow.templates.append(node)
 
     def build(self) -> _ModelCronWorkflow:
-        return _ModelCronWorkflow(
-            api_version=self.api_version,
-            kind=self.kind,
-            metadata=ObjectMeta(
-                annotations=self.annotations,
-                # cluster_name=self.cluster_name,
-                # creation_timestamp=self.creation_timestamp,
-                # deletion_grace_period_seconds=self.deletion_grace_period_seconds,
-                # deletion_timestamp=self.deletion_timestamp,
-                # finalizers=self.finalizers,
-                # generate_name=self.generate_name,
-                # generation=self.generation,
-                # labels=self.labels,
-                # managed_fields=self.managed_fields,
-                # name=self.name,
-                # namespace=self.namespace,
-                # owner_references=self.owner_references,
-                # resource_version=self.resource_version,
-                # self_link=self.self_link,
-                # uid=self.uid,
-            ),
-            spec=CronWorkflowSpec(
-                concurrency_policy=self.concurrency_policy,
-                failed_jobs_history_limit=self.failed_jobs_history_limit,
-                schedule=self.schedule,
-                starting_deadline_seconds=self.starting_deadline_seconds,
-                successful_jobs_history_limit=self.successful_jobs_history_limit,
-                suspend=self.suspend,
-                timezone=self.timezone,
-                workflow_metadata=None,
-                workflow_spec=self.workflow_spec.build(),
-            ),
-            status=self.status,
-        )
+        pass
+        # return _ModelCronWorkflow(
+        #     api_version=self.api_version,
+        #     kind=self.kind,
+        #     metadata=ObjectMeta(
+        #         annotations=self.annotations,
+        #         cluster_name=self.cluster_name,
+        #         creation_timestamp=self.creation_timestamp,
+        #         deletion_grace_period_seconds=self.deletion_grace_period_seconds,
+        #         deletion_timestamp=self.deletion_timestamp,
+        #         finalizers=self.finalizers,
+        #         generate_name=self.generate_name,
+        #         generation=self.generation,
+        #         labels=self.labels,
+        #         managed_fields=self.managed_fields,
+        #         name=self.name,
+        #         namespace=self.namespace,
+        #         owner_references=self.owner_references,
+        #         resource_version=self.resource_version,
+        #         self_link=self.self_link,
+        #         uid=self.uid,
+        #     ),
+        #     spec=CronWorkflowSpec(
+        #         concurrency_policy=self.concurrency_policy,
+        #         failed_jobs_history_limit=self.failed_jobs_history_limit,
+        #         schedule=self.schedule,
+        #         starting_deadline_seconds=self.starting_deadline_seconds,
+        #         successful_jobs_history_limit=self.successful_jobs_history_limit,
+        #         suspend=self.suspend,
+        #         timezone=self.timezone,
+        #         workflow_metadata=None,
+        #         workflow_spec=self.workflow_spec.build(),
+        #     ),
+        #     status=self.status,
+        # )
 
     def to_dict(self) -> Any:
         return self.build().dict(exclude_none=True, by_alias=True)
@@ -105,6 +106,9 @@ class CronWorkflow(ContextMixin):
         if not yaml:
             raise ImportError("PyYAML is not installed")
         return yaml.dump(self.to_dict(), *args, **kwargs)
+    
+    def _add_sub(self, node: Any):
+        pass
 
 
 __all__ = ["CronWorkflow"]
--- src/hera/workflows/workflow_template.py
+++ src/hera/workflows/workflow_template.py
@@ -1,8 +1,25 @@
+from hera.workflows.models import (
+    Workflow as _ModelWorkflow,
+    WorkflowTemplateCreateRequest,
+    WorkflowTemplateLintRequest,
+)
 from hera.workflows.workflow import Workflow
 
 
 class WorkflowTemplate(Workflow):
-    pass
+    def create(self) -> _ModelWorkflow:
+        assert self.workflows_service, "workflow service not initialized"
+        assert self.namespace, "workflow namespace not defined"
+        return self.workflows_service.create_workflow_template(
+            self.namespace, WorkflowTemplateCreateRequest(workflow=self.build())
+        )
+
+    def lint(self) -> _ModelWorkflow:
+        assert self.workflows_service, "workflow service not initialized"
+        assert self.namespace, "workflow namespace not defined"
+        return self.workflows_service.lint_workflow_template(
+            self.namespace, WorkflowTemplateLintRequest(workflow=self.build())
+        )
 
 
 __all__ = ["WorkflowTemplate"]
