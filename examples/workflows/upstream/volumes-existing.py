1939ea9e6c9928ce5c02e9a9c73d3f502df94edb
Kurt Degiorgio <Degiorgio@users.noreply.github.com>
2023-03-16 15:30:02 +0000

    Added upstream examples for volume (#480)
    Signed-off-by: Kurt Degiorgio <kdegiorgio@bloomberg.net>
    Signed-off-by: Kurt Degiorgio <Degiorgio@users.noreply.github.com>
    Co-authored-by: Kurt Degiorgio <kdegiorgio@bloomberg.net>

---
+++ examples/workflows/upstream/volumes-existing.py
@@ -0,0 +1,29 @@
+from hera.workflows import Container, Workflow, Steps, Step
+from hera.workflows import models as m
+from typing import List
+
+
+def get_container(name: str, args: List[str]) -> Container:
+    """Creates container (alpine:latest) with a mounted volume"""
+    return Container(
+        name=name,
+        image="alpine:latest",
+        command=["sh", "-c"],
+        args=args,
+        volume_mounts=[
+            m.VolumeMount(name="workdir", mount_path="/mnt/vol"),
+        ],
+    )
+
+
+with Workflow(
+    generate_name="volumes-existing-",
+    entrypoint="volumes-existing-example",
+    volumes=[m.Volume(name="workdir", persistent_volume_claim={"claim_name": "my-existing-volume"})],
+) as w:
+    append_to_log = get_container("append-to-accesslog", ["echo accessed at: $(date) | tee -a /mnt/vol/accesslog"])
+    print_log = get_container("print-accesslog", ["echo 'Volume access log:'; cat /mnt/vol/accesslog"])
+
+    with Steps(name="volumes-existing-example") as s:
+        append_to_log(name="generate")
+        print_log(name="print")
