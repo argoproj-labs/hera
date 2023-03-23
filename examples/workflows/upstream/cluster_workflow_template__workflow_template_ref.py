ccb338a747e963d59bd492e792b12e3bdab54176
Elliot Gunton <egunton@bloomberg.net>
2023-03-23 09:23:45 +0000

    Assign workflow directly for workflow template ref examples
    Signed-off-by: GitHub <noreply@github.com>

--- examples/workflows/upstream/cluster_workflow_template__workflow_template_ref.py
+++ examples/workflows/upstream/cluster_workflow_template__workflow_template_ref.py
@@ -3,8 +3,7 @@ from hera.workflows.models import WorkflowTemplateRef
 
 wt_ref = WorkflowTemplateRef(name="cluster-workflow-template-submittable", cluster_scope=True)
 
-with Workflow(
+w = Workflow(
     generate_name="cluster-workflow-template-hello-world-",
     workflow_template_ref=wt_ref,
-) as w:
-    pass
+)
