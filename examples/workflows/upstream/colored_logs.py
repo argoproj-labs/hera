51d309ba79496ab77a59f191b9706e307e004b37
Sambhav Kothari <skothari44@bloomberg.net>
2023-03-26 16:44:42 +0100

    Fix pydantic issues with hera.workflows.Env (#505)
    Signed-off-by: GitHub <noreply@github.com>
    Signed-off-by: Sambhav Kothari <skothari44@bloomberg.net>

---
+++ examples/workflows/upstream/colored_logs.py
@@ -0,0 +1,31 @@
+from hera.workflows import Env, Script, Workflow
+
+
+def colored_logs():
+    import time  # noqa: I001
+    import random
+
+    messages = [
+        "No Color",
+        "\x1b[30m%s\x1b[0m" % "FG Black",
+        "\x1b[32m%s\x1b[0m" % "FG Green",
+        "\x1b[34m%s\x1b[0m" % "FG Blue",
+        "\x1b[36m%s\x1b[0m" % "FG Cyan",
+        "\x1b[41m%s\x1b[0m" % "BG Red",
+        "\x1b[43m%s\x1b[0m" % "BG Yellow",
+        "\x1b[45m%s\x1b[0m" % "BG Magenta",
+    ]
+    for i in range(1, 100):
+        print(random.choice(messages))
+        time.sleep(1)
+
+
+with Workflow(generate_name="colored-logs-") as w:
+    s = Script(
+        name="whalesay",
+        source=colored_logs,
+        image="python:3.7",
+        add_cwd_to_sys_path=False,
+        env=[Env(name="PYTHONUNBUFFERED", value="1")],
+    )
+    w.entrypoint = s.name
