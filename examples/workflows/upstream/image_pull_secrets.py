643b1717ca2da8e03eeb336a54b0fd05586ca812
Flaviu Vadan <flaviuvadan@gmail.com>
2023-03-27 09:21:30 -0600

    format
    Signed-off-by: Flaviu Vadan <flaviuvadan@gmail.com>

--- examples/workflows/upstream/image_pull_secrets.py
+++ examples/workflows/upstream/image_pull_secrets.py
@@ -1,4 +1,4 @@
-from hera.workflows import Workflow, Container
+from hera.workflows import Container, Workflow
 
 with Workflow(generate_name="hello-world-", image_pull_secrets="docker-registry-secret", entrypoint="whalesay") as w:
     Container(name="whalesay", image="docker/whalesay:latest", command=["cowsay"], args=["hello world"])
