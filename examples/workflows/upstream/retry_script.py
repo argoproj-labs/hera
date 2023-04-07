1eda6e67894af3fc09dd3e25448ab25e0fe7a112
lakhan jindam <lakhanj569@gmail.com>
2023-04-07 21:08:48 +0530

    add example workflow for retry script
    Signed-off-by: lakhan jindam <lakhanj569@gmail.com>

---
+++ examples/workflows/upstream/retry_script.py
@@ -0,0 +1,14 @@
+from hera.workflows import Workflow, script, RetryStrategy
+
+
+@script(image="python:alpine3.6", retry_strategy=RetryStrategy(limit=10), add_cwd_to_sys_path=False)
+def retry_script():
+    import random
+    import sys
+
+    exit_code = random.choice([0, 1, 1])
+    sys.exit(exit_code)
+
+
+with Workflow(generate_name="retry-script-", entrypoint="retry-script") as w:
+    retry_script()
