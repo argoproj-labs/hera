162f4afe2984585d8a3240a1c23b15a052b58186
Elliot Gunton <egunton@bloomberg.net>
2023-06-05 15:32:22 +0000

    Add workflow unit tests
    Signed-off-by: Elliot Gunton <egunton@bloomberg.net>

---
+++ tests/test_unit/test_workflow.py
@@ -0,0 +1,25 @@
+from hera.workflows.workflow import Workflow
+from hera.workflows.service import WorkflowsService
+from hera.workflows.models import WorkflowCreateRequest
+
+def test_create(mocker):
+    ws = WorkflowsService(namespace="my-namespace")
+    ws.create_workflow = mocker.MagicMock()
+
+    # Note we set the name to None here, otherwise the workflow will take the name from the returned object
+    ws.create_workflow.return_value.metadata.name = None
+
+    # GIVEN
+    with Workflow(
+        generate_name="w",
+        namespace="my-namespace",
+        workflows_service=ws,
+    ) as w:
+        pass
+
+    # WHEN
+    w.create()
+
+    # THEN
+    built_workflow=w.build()
+    w.workflows_service.create_workflow.assert_called_once_with(WorkflowCreateRequest(workflow=built_workflow), namespace="my-namespace")
