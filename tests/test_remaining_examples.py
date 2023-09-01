044c1da8b6f7abc0c115ad7cafde81654b70bf88
Elliot Gunton <egunton@bloomberg.net>
2023-09-01 21:54:56 +0000

    Add fetcher for all upstream yamls
    * Will make it easier than going to GitHub manually for each one,
    plus we can do a Hera round trip "from_yaml" on them
    
    Signed-off-by: Elliot Gunton <egunton@bloomberg.net>

--- tests/test_remaining_examples.py
+++ tests/test_remaining_examples.py
@@ -1,20 +1,38 @@
+from pathlib import Path
 import pkgutil
+from typing import List
 
 import examples.workflows.upstream as hera_upstream_examples
 import pytest
 import requests
 
+from tests.test_examples import CI_MODE, HERA_REGENERATE
+
+ARGO_REPO_URL = "https://raw.githubusercontent.com/argoproj/argo-workflows/master/examples"
 GITHUB_API_ARGO = "https://api.github.com/repos/argoproj/argo-workflows/git/trees/master?recursive=1"
 
 
-@pytest.mark.xfail(reason="Dev tool test")
+def save_upstream_examples(argo_examples: List[str]) -> None:
+    for example in argo_examples:
+        output_file = example.replace("/", "__") + ".upstream.yaml"
+        output_path = Path(f"examples/workflows/upstream/{output_file}")
+
+        if HERA_REGENERATE or not output_path.exists():
+            output_path.write_text(requests.get(f"{ARGO_REPO_URL}/{example}.yaml").text)
+            print(f"Output file at {output_file}")
+        else:
+            print(f"Skipped {example}")
+
+
+@pytest.mark.skipif(CI_MODE, reason="Dev tool test")
 def test_for_missing_examples():
     repo_json = requests.get(GITHUB_API_ARGO).json()
     argo_examples = [
         file["path"] for file in repo_json["tree"] if "examples/" in file["path"] and ".yaml" in file["path"]
     ]
 
-    argo_examples = map(lambda file: file[len("examples/") :][: -len(".yaml")], argo_examples)
+    argo_examples = [file[len("examples/") :][: -len(".yaml")] for file in argo_examples]
+    save_upstream_examples(argo_examples)
 
     hera_examples = [name for _, name, _ in pkgutil.iter_modules(hera_upstream_examples.__path__)]
     hera_examples = list(map(lambda file: file.replace("__", "/").replace("_", "-"), hera_examples))
