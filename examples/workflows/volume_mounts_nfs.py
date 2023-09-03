f196468559c1bf6606400c504de1520d9004d4f2
Junil Kim <junil.kim@qraftec.com>
2023-09-03 17:39:52 +0900

    Add missing argument to NFSVolume build (#757)
    **Pull Request Checklist**
    - [x] Fixes #756
    - [ ] Tests added
    - [ ] Documentation/examples added
    - [ ] [Good commit messages](https://cbea.ms/git-commit/) and/or PR
    title
    
    **Description of PR**
    This PR /fixes missing required argument 'server' when build NFSVolume
    
    ---------
    
    Signed-off-by: KimJunil <junil.kim@qraftec.com>
    Co-authored-by: Sambhav Kothari <skothari44@bloomberg.net>

---
+++ examples/workflows/volume_mounts_nfs.py
@@ -0,0 +1,38 @@
+from hera.workflows import (
+    DAG,
+    Parameter,
+    NFSVolume,
+    Workflow,
+    models as m,
+    script,
+)
+
+
+@script(
+    inputs=Parameter(name="vol"),
+    volume_mounts=[m.VolumeMount(name="{{inputs.parameters.vol}}", mount_path="/mnt/nfs")],
+)
+def foo():
+    import os
+    import subprocess
+
+    print(os.listdir("/mnt"))
+    print(subprocess.run("cd /mnt && df -h", shell=True, capture_output=True).stdout.decode())
+
+
+with Workflow(
+    generate_name="volumes-",
+    volumes=[
+        NFSVolume(
+            name="nfs-volume",
+            server="your.nfs.server",
+            mount_path="/mnt/nfs",
+            path="/share/nfs",
+            size="1Gi",
+            storage_class_name="nfs-client",
+        )
+    ],
+    entrypoint="d",
+) as w:
+    with DAG(name="d"):
+        foo(name="v1", arguments=Parameter(name="vol", value="nfs-volume"))
