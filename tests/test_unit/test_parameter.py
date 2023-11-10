9c57bea2cd8d0e9d20eb085e2688dd8a835ae06b
Elliot Gunton <egunton@bloomberg.net>
2023-11-10 08:18:10 +0000

    Fix enum bool serialization and add tests for all serialized values (#857)
    **Pull Request Checklist**
    - [x] Fixes #706 
    - [x] Tests added
    - [x] Documentation/examples added
    - [x] [Good commit messages](https://cbea.ms/git-commit/) and/or PR
    title
    
    Signed-off-by: Elliot Gunton <egunton@bloomberg.net>

--- tests/test_unit/test_parameter.py
+++ tests/test_unit/test_parameter.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+from hera.workflows.models import Parameter as ModelParameter
 from hera.workflows.parameter import Parameter
 
 
@@ -40,3 +41,70 @@ def test_parameter_no_name_fails_as_output():
         param.as_output()
 
     assert "name cannot be `None` or empty when used" in str(e.value)
+
+
+@pytest.mark.parametrize(
+    "value,default,enum,expected_built_parameter",
+    (
+        pytest.param(
+            "value-string",
+            "default-string",
+            ["value-string", "default-string", "another-enum-string"],
+            ModelParameter(
+                name="test",
+                value="value-string",
+                default="default-string",
+                enum=["value-string", "default-string", "another-enum-string"],
+            ),
+            id="strings",
+        ),
+        pytest.param(
+            1,
+            2,
+            [1, 2],
+            ModelParameter(name="test", value="1", default="2", enum=["1", "2"]),
+            id="numbers",
+        ),
+        pytest.param(
+            True,
+            False,
+            [True, False, None],
+            ModelParameter(name="test", value="true", default="false", enum=["true", "false", "null"]),
+            id="bools-and-none",
+        ),
+        pytest.param(
+            {"my-key": "my-value"},
+            {"my-key": "my-default"},
+            [{"my-key": "my-value"}, {"my-key": "my-default"}],
+            ModelParameter(
+                name="test",
+                value='{"my-key": "my-value"}',
+                default='{"my-key": "my-default"}',
+                enum=['{"my-key": "my-value"}', '{"my-key": "my-default"}'],
+            ),
+            id="arbitrary-dict",
+        ),
+        pytest.param(
+            ["my-value1", "my-value2"],
+            ["my-default-value"],
+            [["my-value1", "my-value2"], ["my-default-value"], ["another", "enum", "value"]],
+            ModelParameter(
+                name="test",
+                value='["my-value1", "my-value2"]',
+                default='["my-default-value"]',
+                enum=['["my-value1", "my-value2"]', '["my-default-value"]', '["another", "enum", "value"]'],
+            ),
+            id="arbitrary-list",
+        ),
+    ),
+)
+def test_parameter_values_serialization(value, default, enum, expected_built_parameter):
+    assert (
+        Parameter(
+            name="test",
+            value=value,
+            default=default,
+            enum=enum,
+        ).as_input()
+        == expected_built_parameter
+    )
