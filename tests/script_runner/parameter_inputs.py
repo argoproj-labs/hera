c17c050140fecf4ddf8921f034246352be95b599
Sambhav Kothari <skothari44@bloomberg.net>
2023-12-15 13:29:59 +0000

    Add optional support for pydantic v1 models when v2 is installed
    Signed-off-by: Sambhav Kothari <skothari44@bloomberg.net>

--- tests/script_runner/parameter_inputs.py
+++ tests/script_runner/parameter_inputs.py
@@ -8,8 +8,13 @@ except ImportError:
 
 from pydantic import BaseModel
 
+try:
+    from pydantic.v1 import BaseModel as V1BaseModel
+except (ImportError, ModuleNotFoundError):
+    from pydantic import BaseModel as V1BaseModel
+
 from hera.shared import global_config
-from hera.workflows import Parameter, script
+from hera.workflows import Parameter, RunnerScriptConstructor, script
 
 global_config.experimental_features["script_annotations"] = True
 
@@ -23,6 +28,15 @@ class Output(BaseModel):
     output: List[Input]
 
 
+class InputV1(V1BaseModel):
+    a: int
+    b: str = "foo"
+
+
+class OutputV1(V1BaseModel):
+    output: List[InputV1]
+
+
 @script()
 def annotated_basic_types(
     a_but_kebab: Annotated[int, Parameter(name="a-but-kebab")] = 2,
@@ -34,6 +48,11 @@ def annotated_basic_types(
 @script()
 def annotated_object(annotated_input_value: Annotated[Input, Parameter(name="input-value")]) -> Output:
     return Output(output=[annotated_input_value])
+
+
+@script(constructor=RunnerScriptConstructor(pydantic_mode=1))
+def annotated_object_v1(annotated_input_value: Annotated[InputV1, Parameter(name="input-value")]) -> OutputV1:
+    return OutputV1(output=[annotated_input_value])
 
 
 @script()
