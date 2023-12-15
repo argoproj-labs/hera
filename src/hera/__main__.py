170163e59a6ee626421286a81f133098718baaf7
DanCardin <ddcardin@gmail.com>
2023-12-15 13:08:46 -0500

    chore: Add warning to indicate that the CLI is experimental.
    Signed-off-by: DanCardin <ddcardin@gmail.com>

--- src/hera/__main__.py
+++ src/hera/__main__.py
@@ -8,7 +8,13 @@ def main():
     except ImportError:
         raise ImportError("Use of the `hera` CLI tool requires installing the 'cli' extra, `pip install hera[cli]`.")
 
+    import rich
+
     from hera._cli.base import Hera
+
+    rich.print(
+        "[yellow bold]warning: The `hera` CLI is a currently work-in-progress, subject to change at any time![/yellow bold]"
+    )
 
     cappa.invoke(Hera)
 
