3a3aa2ed5c7bd16346010128a436aa1717d4db54
Elliot Gunton <egunton@bloomberg.net>
2024-01-18 15:15:27 +0000

    Remove unused replace_keys function
    Signed-off-by: Elliot Gunton <egunton@bloomberg.net>

--- src/hera/workflows/io.py
+++ src/hera/workflows/io.py
@@ -1,6 +1,6 @@
 """Input/output models for the Hera runner. TODO move to hera.workflows.runner package."""
 from collections import ChainMap
-from typing import Any, Dict, List, Tuple, Type, Union
+from typing import Any, List, Union
 
 from hera.shared._pydantic import BaseModel
 from hera.workflows.artifact import Artifact
@@ -15,10 +15,6 @@ try:
     from typing import Annotated, get_args, get_origin  # type: ignore
 except ImportError:
     from typing_extensions import Annotated, get_args, get_origin  # type: ignore
-
-
-def _is_annotated_as(annotation: Any, class_or_tuple: Union[Type, Tuple[Type, ...]]):
-    return get_origin(annotation) is Annotated and isinstance(get_args(annotation)[1], class_or_tuple)
 
 
 class RunnerInput(BaseModel):
@@ -88,15 +84,3 @@ class RunnerOutput(BaseModel):
 
         # Create a Parameter from basic type annotations
         return Parameter(name=field_name, default=cls.__fields__[field_name].default)
-
-    @classmethod
-    def replace_keys(cls, obj: Dict) -> Dict:
-        """Replaces keys in obj with Annotated Parameter/Artifact names for Argo."""
-        for key, annotation in ChainMap(*(get_annotations(c) for c in cls.__mro__)).items():
-            if key in {"exit_code", "result"} or not _is_annotated_as(annotation, (Artifact, Parameter)):
-                continue
-            argo_name = get_args(annotation)[1].name
-
-            obj[argo_name] = obj[key]
-            del obj[key]
-        return obj
