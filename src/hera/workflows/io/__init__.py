6dafbb651e2926231a972ce43aaa2ab0e82057c0
Elliot Gunton <egunton@bloomberg.net>
2024-02-12 12:08:59 +0000

    Add __init__ file for io module
    Signed-off-by: Elliot Gunton <egunton@bloomberg.net>

---
+++ src/hera/workflows/io/__init__.py
@@ -0,0 +1,12 @@
+"""Hera IO models."""
+from importlib.util import find_spec
+
+if find_spec("pydantic.v1"):
+    from hera.workflows.io.v2 import RunnerInput, RunnerOutput
+else:
+    from hera.workflows.io.v1 import RunnerInput, RunnerOutput  # type: ignore
+
+__all__ = [
+    "RunnerInput",
+    "RunnerOutput",
+]
