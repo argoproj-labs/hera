7e41ed0c749791af671febe0ad78f8f91a004e03
Ukjae Jeong <jeongukjae@gmail.com>
2024-08-20 11:14:26 +0100

    Add default value to optional parameter cases
    Signed-off-by: Ukjae Jeong <jeongukjae@gmail.com>

--- tests/script_runner/parameter_with_complex_types.py
+++ tests/script_runner/parameter_with_complex_types.py
@@ -8,27 +8,27 @@ global_config.experimental_features["script_annotations"] = True
 
 
 @script(constructor="runner")
-def optional_str_parameter(my_string: Optional[str]) -> Optional[str]:
+def optional_str_parameter(my_string: Optional[str] = None) -> Optional[str]:
     return my_string
 
 
 @script(constructor="runner")
-def optional_str_parameter_using_union(my_string: Union[None, str]) -> Union[None, str]:
+def optional_str_parameter_using_union(my_string: Union[None, str] = None) -> Union[None, str]:
     return my_string
 
 
 if sys.version_info[0] >= 3 and sys.version_info[1] >= 10:
     # Union types using OR operator are allowed since python 3.10.
     @script(constructor="runner")
-    def optional_str_parameter_using_or(my_string: str | None) -> str | None:
+    def optional_str_parameter_using_or(my_string: str | None = None) -> str | None:
         return my_string
 
 
 @script(constructor="runner")
-def optional_int_parameter(my_int: Optional[int]) -> Optional[int]:
+def optional_int_parameter(my_int: Optional[int] = None) -> Optional[int]:
     return my_int
 
 
 @script(constructor="runner")
-def union_parameter(my_param: Union[str, int]) -> Union[str, int]:
+def union_parameter(my_param: Union[str, int] = None) -> Union[str, int]:
     return my_param
