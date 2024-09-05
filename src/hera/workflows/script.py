4f9f822934db120aba968d5021a2ba75e739cebd
Ukjae Jeong <JeongUkJae@gmail.com>
2024-09-05 11:57:30 +0100

    Apply suggestions from code review
    Co-authored-by: Elliot Gunton <elliotgunton@gmail.com>
    Signed-off-by: Ukjae Jeong <JeongUkJae@gmail.com>

--- src/hera/workflows/script.py
+++ src/hera/workflows/script.py
@@ -558,11 +558,12 @@ def _extract_return_annotation_output(source: Callable) -> List:
         workflow_args = [
             get_args(annotated_type) for annotated_type in annotation_args if get_workflow_annotation(annotated_type)
         ]
+        # If all tuple elements are annotated as Parameter/Artifact
         if len(workflow_args) == len(annotation_args):
             output.extend(workflow_args)
-        elif workflow_args:
+        elif workflow_args:  # Only some tuple elements are annotated as Parameter/Artifact
             raise ValueError(
-                f"Function '{source.__name__}' output has partially annotated return type. Hera allows tuple annotation to be fully annotated or not."
+                f"Function '{source.__name__}' output has partially annotated tuple return type. Tuple elements must be all Annotated as Parameter/Artifact, or contain no Parameter/Artifact annotations for a raw tuple return type."
             )
     elif (
         origin_type is None
