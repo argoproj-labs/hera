246132668dabd1aaf1a1aea6d4e635ff94afe916
Alice Purcell <alicederyn@gmail.com>
2024-10-14 14:11:26 +0100

    Move with_param examples into script_annotations tests
    Signed-off-by: Alice Purcell <alicederyn@gmail.com>

--- tests/script_annotations/with_param.py
+++ tests/script_annotations/with_param.py
@@ -1,9 +1,3 @@
-"""
-This example showcases how clients can use Hera to dynamically generate tasks that process outputs from one task in
-parallel. This is useful for batch jobs and instances where clients do not know ahead of time how many tasks/entities
-they may need to process.
-"""
-
 from typing import Annotated, List
 
 from hera.shared import global_config
@@ -22,9 +16,8 @@ def consume(some_value: Annotated[int, Parameter(name="some-value", description=
     print("Received value: {value}!".format(value=some_value))
 
 
-# assumes you used `hera.set_global_token` and `hera.set_global_host` so that the workflow can be submitted
 with Workflow(generate_name="dynamic-fanout-", entrypoint="d") as w:
-    with DAG(name="d"):
+    with DAG(name="dag"):
         g = generate(arguments={})
         c = consume(with_param=g.get_parameter("some-values"))
         g >> c
