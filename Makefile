031af5203aaf62230077f5cb0b5b878c2306f5ad
Elliot Gunton <elliotgunton@gmail.com>
2024-12-04 17:10:23 +0000

    Add on-cluster test for DAG artifact example
    * Tidy up Makefile targets
    * Add set-up-artifacts target to add minio storage from the argoproj-labs training materials
    
    Signed-off-by: Elliot Gunton <elliotgunton@gmail.com>

--- Makefile
+++ Makefile
@@ -147,7 +147,7 @@ install-k3d: ## Install k3d client
 	curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 .PHONY: install-argo
-install-argo:  ## Install argo client
+install-argo:  ## Install argo CLI client
 	# Download the binary
 	curl -sLO https://github.com/argoproj/argo-workflows/releases/download/v$(ARGO_WORKFLOWS_VERSION)/argo-linux-amd64.gz
 
@@ -163,15 +163,24 @@ install-argo:  ## Install argo client
 	# Test installation
 	argo version
 
-.PHONY: run-argo
-run-argo: ## Start the argo server
+.PHONY: set-up-cluster
+set-up-cluster: ## Create the cluster and argo namespace
 	k3d cluster list | grep test-cluster || k3d cluster create test-cluster
 	k3d kubeconfig merge test-cluster --kubeconfig-switch-context
 	kubectl get namespace argo || kubectl create namespace argo
+
+.PHONY: run-argo
+run-argo: ## Start the argo service
 	kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/download/v$(ARGO_WORKFLOWS_VERSION)/install.yaml
 	kubectl patch deployment argo-server --namespace argo --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/args", "value": ["server", "--auth-mode=server"]}]'
 	kubectl create rolebinding default-admin --clusterrole=admin --serviceaccount=argo:default --namespace=argo
 	kubectl rollout status -n argo deployment/argo-server --timeout=120s --watch=true
+
+.PHONY: set-up-artifacts
+set-up-artifacts: ## Adds minio for running examples with artifact storage
+	kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj-labs/training-material/main/config/minio/minio.yaml
+	kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj-labs/training-material/main/config/argo-workflows/workflows-controller-configmap.yaml
+	kubectl apply -n argo -f tests/submissions/roles.yaml
 
 .PHONY: stop-argo
 stop-argo:  ## Stop the argo server
---
+++ tests/submissions/test_dag_artifacts.py
@@ -0,0 +1,55 @@
+import pytest
+
+from hera.workflows import DAG, Artifact, Container, NoneArchiveStrategy, Workflow, WorkflowsService
+
+
+def get_workflow() -> Workflow:
+    with Workflow(
+        generate_name="dag-artifact-passing-",
+        entrypoint="runner-dag",
+        namespace="argo",
+        workflows_service=WorkflowsService(
+            host="https://localhost:2746",
+            namespace="argo",
+            verify_ssl=False,
+        ),
+    ) as w:
+        hello_world_to_file = Container(
+            name="hello-world-to-file",
+            image="busybox",
+            command=["sh", "-c"],
+            args=["sleep 1; echo hello world | tee /tmp/hello_world.txt"],
+            outputs=[Artifact(name="hello-art", path="/tmp/hello_world.txt", archive=NoneArchiveStrategy())],
+        )
+        print_message_from_file = Container(
+            name="print-message-from-file",
+            image="alpine:latest",
+            command=["sh", "-c"],
+            args=["cat /tmp/message"],
+            inputs=[Artifact(name="message", path="/tmp/message")],
+        )
+
+        with DAG(
+            name="generate-artifact-dag",
+            outputs=[Artifact(name="hello-file", from_="{{tasks.hello-world-to-file.outputs.artifacts.hello-art}}")],
+        ) as d1:
+            hello_world_to_file()
+
+        with DAG(name="consume-artifact-dag", inputs=[Artifact(name="hello-file-input")]) as d2:
+            print_message_from_file(
+                arguments=d2.get_artifact("hello-file-input").with_name("message"),
+            )
+
+        with DAG(name="runner-dag"):
+            generator_dag = d1()
+            consumer_dag = d2(arguments=generator_dag.get_artifact("hello-file").with_name("hello-file-input"))
+
+            generator_dag >> consumer_dag
+
+    return w
+
+
+@pytest.mark.on_cluster
+def test_dag_artifacts():
+    model_workflow = get_workflow().create(wait=True)
+    assert model_workflow.status and model_workflow.status.phase == "Succeeded"
