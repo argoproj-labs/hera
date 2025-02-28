3705be31a60665e5d8f54408be6f36030d833bb4
Elliot Gunton <elliotgunton@gmail.com>
2025-02-28 22:54:03 +0000

    Allow Artifacts in argument dictionaries
    Signed-off-by: Elliot Gunton <elliotgunton@gmail.com>

--- tests/test_unit/test_arguments.py
+++ tests/test_unit/test_arguments.py
@@ -3,8 +3,10 @@
 import pytest
 
 from hera.workflows._mixins import ArgumentsMixin
+from hera.workflows.artifact import Artifact
 from hera.workflows.models import (
     Arguments as ModelArguments,
+    Artifact as ModelArtifact,
     Parameter as ModelParameter,
 )
 from hera.workflows.parameter import Parameter
@@ -92,6 +94,210 @@ from hera.workflows.parameter import Parameter
     ),
 )
 def test_argument_parameter_build(arguments, expected_built_arguments):
+    assert (
+        ArgumentsMixin(
+            arguments=arguments,
+        )._build_arguments()
+        == expected_built_arguments
+    )
+
+
+model_artifact = ModelArtifact(name="artifact-name", from_="somewhere")
+
+
+@pytest.mark.parametrize(
+    "arguments,expected_built_arguments",
+    (
+        pytest.param(
+            ModelArtifact(
+                name="artifact-name",
+                from_="somewhere",
+            ),
+            ModelArguments(
+                artifacts=[
+                    ModelArtifact(
+                        name="artifact-name",
+                        from_="somewhere",
+                    )
+                ],
+            ),
+            id="single-model-artifact",
+        ),
+        pytest.param(
+            [
+                ModelArtifact(
+                    name="artifact-name-1",
+                    from_="somewhere",
+                ),
+                ModelArtifact(
+                    name="artifact-name-2",
+                    from_="somewhere",
+                ),
+            ],
+            ModelArguments(
+                artifacts=[
+                    ModelArtifact(
+                        name="artifact-name-1",
+                        from_="somewhere",
+                    ),
+                    ModelArtifact(
+                        name="artifact-name-2",
+                        from_="somewhere",
+                    ),
+                ],
+            ),
+            id="list-model-artifacts",
+        ),
+        pytest.param(
+            {"a-key": ModelArtifact(name="artifact-name", from_="somewhere")},
+            ModelArguments(
+                artifacts=[
+                    ModelArtifact(
+                        name="a-key",
+                        from_="somewhere",
+                    )
+                ],
+            ),
+            id="model-artifact-in-dict",
+        ),
+        pytest.param(
+            [
+                {
+                    "a-key": model_artifact,
+                },
+                model_artifact,
+            ],
+            ModelArguments(
+                artifacts=[
+                    ModelArtifact(
+                        name="a-key",
+                        from_="somewhere",
+                    ),
+                    ModelArtifact(
+                        name="artifact-name",
+                        from_="somewhere",
+                    ),
+                ],
+            ),
+            id="do-not-rename-original-artifact-object",
+        ),
+        pytest.param(
+            {"a-key": Artifact(name="artifact-name", from_="somewhere").with_name("ignore-me")},
+            ModelArguments(
+                artifacts=[
+                    ModelArtifact(
+                        name="a-key",
+                        from_="somewhere",
+                    )
+                ],
+            ),
+            id="hera-artifact-ignore-alt-name",
+        ),
+    ),
+)
+def test_argument_artifact_build(arguments, expected_built_arguments):
+    assert (
+        ArgumentsMixin(
+            arguments=arguments,
+        )._build_arguments()
+        == expected_built_arguments
+    )
+
+
+@pytest.mark.parametrize(
+    "arguments,expected_built_arguments",
+    (
+        pytest.param(
+            None,
+            None,
+            id="no-arguments",
+        ),
+        pytest.param(
+            ModelArguments(),
+            ModelArguments(),
+            id="model-arguments",
+        ),
+        pytest.param(
+            [Parameter(name="param-name", value="a-value"), Artifact(name="artifact-name", from_="somewhere")],
+            ModelArguments(
+                parameters=[
+                    ModelParameter(
+                        name="param-name",
+                        value="a-value",
+                    )
+                ],
+                artifacts=[
+                    ModelArtifact(
+                        name="artifact-name",
+                        from_="somewhere",
+                    )
+                ],
+            ),
+            id="mixed-list",
+        ),
+        pytest.param(
+            {"param-name": "a-value", "a-key": ModelArtifact(name="artifact-name", from_="somewhere")},
+            ModelArguments(
+                parameters=[
+                    ModelParameter(
+                        name="param-name",
+                        value="a-value",
+                    )
+                ],
+                artifacts=[
+                    ModelArtifact(
+                        name="a-key",
+                        from_="somewhere",
+                    )
+                ],
+            ),
+            id="mixed-dict",
+        ),
+        pytest.param(
+            [
+                {"param-name": "a-value"},
+                ModelArtifact(name="artifact-name", from_="somewhere"),
+            ],
+            ModelArguments(
+                parameters=[
+                    ModelParameter(
+                        name="param-name",
+                        value="a-value",
+                    )
+                ],
+                artifacts=[
+                    ModelArtifact(
+                        name="artifact-name",
+                        from_="somewhere",
+                    )
+                ],
+            ),
+            id="param-dict-in-list",
+        ),
+        pytest.param(
+            [
+                {"param-name": "a-value"},
+                {"a-key": Artifact(name="artifact-name", from_="somewhere").with_name("ignore-me")},
+            ],
+            ModelArguments(
+                parameters=[
+                    ModelParameter(
+                        name="param-name",
+                        value="a-value",
+                    )
+                ],
+                artifacts=[
+                    ModelArtifact(
+                        name="a-key",
+                        from_="somewhere",
+                    )
+                ],
+            ),
+            id="multiple-dicts-in-list",
+        ),
+    ),
+)
+def test_mixed_arguments_build(arguments, expected_built_arguments):
     assert (
         ArgumentsMixin(
             arguments=arguments,
