a658131fd236fc1f3b9d29d0f8ed380d5f315040
Elliot Gunton <elliotgunton@gmail.com>
2025-08-25 09:52:20 +0100

    Add async functions to Workflow classes
    * Add test for async Workflow creation
    
    Signed-off-by: Elliot Gunton <elliotgunton@gmail.com>

--- src/hera/workflows/cluster_workflow_template.py
+++ src/hera/workflows/cluster_workflow_template.py
@@ -2,6 +2,7 @@
 
 from hera.exceptions import NotFound
 from hera.shared._pydantic import validator
+from hera.workflows.async_service import AsyncWorkflowsService
 from hera.workflows.models import (
     ClusterWorkflowTemplate as _ModelClusterWorkflowTemplate,
     ClusterWorkflowTemplateCreateRequest,
@@ -9,6 +10,7 @@ from hera.workflows.models import (
     ClusterWorkflowTemplateUpdateRequest,
 )
 from hera.workflows.protocol import TWorkflow
+from hera.workflows.service import WorkflowsService
 from hera.workflows.workflow_template import WorkflowTemplate
 
 
@@ -25,14 +27,14 @@ class ClusterWorkflowTemplate(WorkflowTemplate):
 
     def create(self) -> TWorkflow:  # type: ignore
         """Creates the ClusterWorkflowTemplate on the Argo cluster."""
-        assert self.workflows_service, "workflow service not initialized"
+        assert isinstance(self.workflows_service, WorkflowsService), "workflows service not initialized"
         return self.workflows_service.create_cluster_workflow_template(
             ClusterWorkflowTemplateCreateRequest(template=self.build())  # type: ignore
         )
 
     def get(self) -> TWorkflow:
         """Attempts to get a workflow template based on the parameters of this template e.g. name + namespace."""
-        assert self.workflows_service, "workflow service not initialized"
+        assert isinstance(self.workflows_service, WorkflowsService), "workflows service not initialized"
         assert self.name, "workflow name not defined"
         return self.workflows_service.get_cluster_workflow_template(name=self.name)
 
@@ -43,7 +45,7 @@ class ClusterWorkflowTemplate(WorkflowTemplate):
         a get prior to updating to get the resource version to update in the first place. If you know the template
         does not exist ahead of time, it is more efficient to use `create()` directly to avoid one round trip.
         """
-        assert self.workflows_service, "workflow service not initialized"
+        assert isinstance(self.workflows_service, WorkflowsService), "workflows service not initialized"
         assert self.name, "workflow name not defined"
         # we always need to do a get prior to updating to get the resource version to update in the first place
         # https://github.com/argoproj/argo-workflows/pull/5465#discussion_r597797052
@@ -61,8 +63,51 @@ class ClusterWorkflowTemplate(WorkflowTemplate):
 
     def lint(self) -> TWorkflow:
         """Lints the ClusterWorkflowTemplate using the Argo cluster."""
-        assert self.workflows_service, "workflow service not initialized"
+        assert isinstance(self.workflows_service, WorkflowsService), "workflows service not initialized"
         return self.workflows_service.lint_cluster_workflow_template(
+            ClusterWorkflowTemplateLintRequest(template=self.build())  # type: ignore
+        )
+
+    async def async_create(self) -> TWorkflow:  # type: ignore
+        """Creates the ClusterWorkflowTemplate on the Argo cluster."""
+        assert isinstance(self.workflows_service, AsyncWorkflowsService), "workflows service not initialized"
+        return await self.workflows_service.create_cluster_workflow_template(
+            ClusterWorkflowTemplateCreateRequest(template=self.build())  # type: ignore
+        )
+
+    async def async_get(self) -> TWorkflow:
+        """Attempts to get a workflow template based on the parameters of this template e.g. name + namespace."""
+        assert isinstance(self.workflows_service, AsyncWorkflowsService), "workflows service not initialized"
+        assert self.name, "workflow name not defined"
+        return await self.workflows_service.get_cluster_workflow_template(name=self.name)
+
+    async def async_update(self) -> TWorkflow:
+        """Attempts to perform a workflow template update based on the parameters of this template.
+
+        Note that this creates the template if it does not exist. In addition, this performs
+        a get prior to updating to get the resource version to update in the first place. If you know the template
+        does not exist ahead of time, it is more efficient to use `create()` directly to avoid one round trip.
+        """
+        assert isinstance(self.workflows_service, AsyncWorkflowsService), "workflows service not initialized"
+        assert self.name, "workflow name not defined"
+        # we always need to do a get prior to updating to get the resource version to update in the first place
+        # https://github.com/argoproj/argo-workflows/pull/5465#discussion_r597797052
+
+        template = self.build()
+        try:
+            curr = await self.async_get()
+            template.metadata.resource_version = curr.metadata.resource_version
+        except NotFound:
+            return await self.async_create()
+        return await self.workflows_service.update_cluster_workflow_template(
+            self.name,
+            ClusterWorkflowTemplateUpdateRequest(template=template),  # type: ignore
+        )
+
+    async def async_lint(self) -> TWorkflow:
+        """Lints the ClusterWorkflowTemplate using the Argo cluster."""
+        assert isinstance(self.workflows_service, AsyncWorkflowsService), "workflows service not initialized"
+        return await self.workflows_service.lint_cluster_workflow_template(
             ClusterWorkflowTemplateLintRequest(template=self.build())  # type: ignore
         )
 
