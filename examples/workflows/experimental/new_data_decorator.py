cf5d5654b54c2a18239d567b4550791ee6e2ec97
Ukjae Jeong <jeongukjae@gmail.com>
2025-09-02 14:10:44 +0100

    Add examples
    Signed-off-by: Ukjae Jeong <jeongukjae@gmail.com>

---
+++ examples/workflows/experimental/new_data_decorator.py
@@ -0,0 +1,38 @@
+"""This example shows the use of the data decorator and special Input/Output classes."""
+
+from typing_extensions import Annotated
+
+from hera.expr import g, it
+from hera.shared import global_config
+from hera.workflows import (
+    Artifact,
+    Input,
+    Output,
+    S3Artifact,
+    Workflow,
+)
+
+global_config.experimental_features["decorator_syntax"] = True
+
+
+# We start by defining our Workflow
+w = Workflow(generate_name="data-workflow-")
+
+
+# This defines the template's inputs
+class MyInput(Input):
+    bucket_name: str = "my-bucket"
+
+
+class MyOutput(Output):
+    file: Annotated[str, Artifact(path="/file")]
+
+
+# We then use the decorators of the `Workflow` object
+# to set the entrypoint and create a Data template
+@w.set_entrypoint
+@w.data(
+    source=S3Artifact(name="test-bucket", bucket=f"{g.inputs.parameters.bucket_name:$}"),
+    transformations=[g.data.filter(it.ends_with("main.log"))],  # type: ignore
+)
+def basic_hello_world(my_input: MyInput) -> MyOutput: ...
---
+++ examples/workflows/experimental/new_suspend_decorator.py
@@ -0,0 +1,43 @@
+"""This example shows the use of the suspend decorator and special Input/Output classes."""
+
+from typing_extensions import Annotated
+
+from hera.shared import global_config
+from hera.workflows import Input, Output, Parameter, Workflow
+from hera.workflows.models import SuppliedValueFrom, ValueFrom
+
+global_config.experimental_features["decorator_syntax"] = True
+
+
+# We start by defining our Workflow
+w = Workflow(generate_name="suspend-workflow-")
+
+
+# This defines the template's inputs
+class MyInput(Input):
+    approve: Annotated[
+        str,
+        Parameter(
+            description="Choose YES to continue workflow and deploy to production",
+            enum=["YES", "NO"],
+        ),
+    ] = "NO"
+
+
+class MyOutput(Output):
+    approve: Annotated[
+        str,
+        Parameter(
+            name="approve",
+            value_from=ValueFrom(
+                supplied=SuppliedValueFrom(),
+            ),
+        ),
+    ]
+
+
+# We then use the decorators of the `Workflow` object
+# to set the entrypoint and create a Resouruce template
+@w.set_entrypoint
+@w.suspend_template()
+def basic_hello_world(my_input: MyInput) -> MyOutput: ...
