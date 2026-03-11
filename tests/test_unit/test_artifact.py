fd865723a16e7976772637b9d939837b67c07c2c
Elliot Gunton <elliotgunton@gmail.com>
2026-03-11 21:19:08 +0000

    fixup! Remove deprecated code
    Signed-off-by: Elliot Gunton <elliotgunton@gmail.com>

--- tests/test_unit/test_artifact.py
+++ tests/test_unit/test_artifact.py
@@ -3,7 +3,6 @@
 import pytest
 
 from hera.workflows.artifact import Artifact
-from hera.workflows.models import Artifact as ModelArtifact
 
 
 def test_artifact_no_name_can_be_created():
@@ -23,20 +22,6 @@ def test_artifact_no_name_fails_build_artifact_paths():
     artifact = Artifact(path="/tmp/path")
     with pytest.raises(ValueError) as e:
         artifact._build_artifact_paths()
-
-    assert "name cannot be `None` or empty when used" in str(e.value)
-
-
-def test_artifact_as_name():
-    """Checking deprecated function still works (and for coverage)."""
-    artifact = Artifact(name="test", path="/tmp/path")
-    assert artifact.as_name("new") == ModelArtifact(name="new", path="/tmp/path")
-
-
-def test_artifact_no_name_fails_as_name():
-    artifact = Artifact(path="/tmp/path")
-    with pytest.raises(ValueError) as e:
-        artifact.as_name("my_art")
 
     assert "name cannot be `None` or empty when used" in str(e.value)
 
